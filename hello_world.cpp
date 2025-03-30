676add57ae9c1b378f2b89924897c81f1aa697a6
merkury9testgitlab <merkury9testgithub@gmail.com>
2025-03-30 22:40:19 +0500

    Добавлены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int main() {
+	// Имя.
 	std::string name;
 	
+	// Ввод имени.
 	std::cout << "Name ->";
 	std::cin >> name;
 	
+	// Вывод результата.
 	std::cout << "Hello world from @" << name << std::endl;
 		
 	return 0;
